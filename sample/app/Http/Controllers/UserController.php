5a4a13f40eb142ed0dca67dceaf6ccefd540eec0
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-08-26 17:26:25 +0900

    Save data to cache when user created

--- sample/app/Http/Controllers/UserController.php
+++ sample/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class UserController extends Controller
 {
@@ -15,6 +16,8 @@ class UserController extends Controller
         $user = new \App\User();
         $user->name = $name;
         if ($user->save()) {
+            // redisにユーザ情報を保存
+            Redis::set('user:profile:'.$user->id, $user);
             return 'success';
         }
         return 'failed';
@@ -22,6 +25,14 @@ class UserController extends Controller
 
     public function get(Request $request, $id)
     {
+        $cacheResult = Redis::get('user:profile:'.$id);
+
+        // redisキャッシュがあればキャッシュから
+        if ($cacheResult) {
+            return $cacheResult;
+        }
+
+        // なければDBから取得
         return \App\User::findOrFail($id);
     }
 }
--- sample/bootstrap/app.php
+++ sample/bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
-// $app->withFacades();
+$app->withFacades();
 
 $app->withEloquent();
 
@@ -81,6 +81,7 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Illuminate\Redis\RedisServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
